4ed886990983bef989ee8a093251792f17ea114d
Deniel <denielaz2020@gmail.com>
2024-03-10 17:53:05 +0100

    refactor: push twitch stream source

--- src/scenes/chat/helpers/sourcesWithUrl.js
+++ src/scenes/chat/helpers/sourcesWithUrl.js
@@ -1,5 +1,7 @@
 const { getSourceUrl } = require('../../../utils/youtube');
 const { youtubeUrlValidate } = require('../../../utils/validators/youtubeUrlValidate');
+const { twitchUrlValidate } = require('../../../utils/validators/twitchUrlValidate');
+const { getSourceUrlTwitch } = require('../../../utils/twitch');
 
 const sourcesWithUrl = async (resources) => {
 	const sourcesWithVideoUrl = [];
@@ -8,6 +10,12 @@ const sourcesWithUrl = async (resources) => {
 		if (youtubeUrlValidate(dataValues.url)) {
 			const videoSourceUrl = await getSourceUrl(dataValues.url);
 			sourcesWithVideoUrl.push({ ...dataValues, url: videoSourceUrl });
+		} else if (twitchUrlValidate(dataValues.url)) {
+			const data = await getSourceUrlTwitch(dataValues.url);
+			if (data) {
+				const videoSourceUrl = data.urls['720p60'];
+				sourcesWithVideoUrl.push({ ...dataValues, url: videoSourceUrl });
+			}
 		} else {
 			sourcesWithVideoUrl.push({ ...dataValues, url: dataValues.url });
 		}
