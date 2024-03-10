186dd984615de540a7cd423db0f932cabde84318
Deniel <denielaz2020@gmail.com>
2024-03-10 17:50:55 +0100

    refactor: check if stream really started

--- src/scenes/chat/helpers/startStream.js
+++ src/scenes/chat/helpers/startStream.js
@@ -2,9 +2,15 @@ const { deleteMessageWithDelay } = require('../../../utils/deleteMessageWithDela
 const { startStreaming } = require('../../../utils/stream/startStreaming');
 
 const startStream = async (resources, streamKey, ctx) => {
-	startStreaming(resources, streamKey);
-	const msg = await ctx.reply(`Трансляция была запущена!`);
-	deleteMessageWithDelay(ctx, msg.message_id, 3000);
+	const result = startStreaming(resources, streamKey);
+	if (result === true) {
+		const msg = await ctx.reply(`Трансляция была запущена!`);
+		deleteMessageWithDelay(ctx, msg.message_id, 3000);
+		return true;
+	} else {
+		ctx.reply('Транслируемый ресурс не найден! Возможно стример оффлайн или ресурс был удалён');
+		return false;
+	}
 };
 
 module.exports = { startStream };
